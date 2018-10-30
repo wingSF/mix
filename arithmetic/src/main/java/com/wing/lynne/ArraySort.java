eadd5ca06af6672c909554a49bf4f2b2a44cbd8a
wing <18735117232@163.com>
2018-10-30 17:24:19 +0800

    1.提交快速排序注释

--- arithmetic/src/main/java/com/wing/lynne/ArraySort.java
+++ arithmetic/src/main/java/com/wing/lynne/ArraySort.java
@@ -14,20 +14,23 @@ public class ArraySort {
      */
     private static int[] quickSort(int[] array) {
 
+        //1.特殊数组的处理
         if (array == null || array.length == 0 || array.length == 1) {
             return array;
         }
 
+        //2.获取分段中间索引以及索引位的数值
         int length = array.length;
-
         int flag = length / 2;
         int flagNumber = array[flag];
 
+        //3.初始化新数组存放截断产生的新数组
         int[] biggerArray = new int[length - 1];
         int biggerArrayIndex = 0;
         int[] smallerArray = new int[length - 1];
         int smallerArrayIndex = 0;
 
+        //4.遍历旧数组，给新初始化的数组赋值
         for (int i = 0; i < length; i++) {
             if (i == flag) {
                 continue;
@@ -42,23 +45,23 @@ public class ArraySort {
             }
         }
 
+        //5.对新产生的数组进行截取，将0位全部丢弃
         biggerArray = Arrays.copyOfRange(biggerArray, 0, biggerArrayIndex);
         smallerArray = Arrays.copyOfRange(smallerArray, 0, smallerArrayIndex);
 
+        //6.对新产生的数组进行递归处理
         if (biggerArray.length != 0) {
             quickSort(biggerArray);
         }
-
         if (smallerArray.length != 0) {
             quickSort(smallerArray);
         }
 
+        //7.对老数据重新进行赋值操作
         for (int i = 0; i < smallerArray.length; i++) {
             array[i] = smallerArray[i];
         }
-
         array[smallerArray.length] = flagNumber;
-
         for (int i = 0; i < biggerArray.length; i++) {
             array[i + smallerArray.length + 1] = biggerArray[i];
         }
