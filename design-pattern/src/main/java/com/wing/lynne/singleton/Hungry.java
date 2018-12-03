bd327b8f842ef2b3495354a39352598c54c5ed2a
wing <18735117232@163.com>
2018-12-03 09:28:49 +0800

    1.私有构造方法 2.代理模式的基本结构

--- design-pattern/src/main/java/com/wing/lynne/singleton/Hungry.java
+++ design-pattern/src/main/java/com/wing/lynne/singleton/Hungry.java
@@ -10,6 +10,9 @@ public class Hungry {
 
     private static final Hungry instance = new Hungry();
 
+    private Hungry(){
+    }
+
     public static Hungry getInstance() {
         return instance;
     }
