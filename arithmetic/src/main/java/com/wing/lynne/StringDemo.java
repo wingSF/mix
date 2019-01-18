1e99153570be3061c0f3cbc2281e713d352b2438
wing <18735117232@163.com>
2019-01-18 09:20:47 +0800

    1.String Reflect

--- arithmetic/src/main/java/com/wing/lynne/StringDemo.java
+++ arithmetic/src/main/java/com/wing/lynne/StringDemo.java
@@ -1,5 +1,7 @@
 package com.wing.lynne;
 
+import java.lang.reflect.Field;
+
 /**
  * String#intern返回常量池中的该对象
  * 不同版本的字符串写入常量池的逻辑不一致
@@ -9,7 +11,7 @@ package com.wing.lynne;
  */
 public class StringDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
 
 //        注释打开和不打开是俩种结果
 //        jdk版本也会影响返回值（以1.7为分界线）
@@ -20,9 +22,29 @@ public class StringDemo {
         String b = new StringBuffer("ja").append("va").toString();
         String c = new StringBuffer("ty").append("pe").toString();
 
+        System.out.println("-----------------------");
         System.out.println(a.intern() == a);
         System.out.println(b.intern() == b);
         System.out.println(c.intern() == c);
+
+        String value = "wing";
+
+        Field field = String.class.getDeclaredField("value");
+        field.setAccessible(true);
+        field.set(value,"lynne".toCharArray());
+
+        System.out.println("-----------------------");
+        System.out.println(value);
+
+        System.out.println("-----------------------");
+        System.out.println("lynne"==value);
+        System.out.println("wing"==value);
+
+
+        System.out.println("-------- start --------");
+        System.out.println("lynne".equals(value));
+        System.out.println("wing".equals(value));
+        System.out.println("-------- end ----------");
 
     }
 
