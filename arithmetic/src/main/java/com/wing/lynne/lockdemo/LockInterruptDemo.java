afd6332efb81438d18c80fc796ab1248f7c2d85c
wing <18735117232@163.com>
2019-02-26 22:44:13 +0800

    增加lock 中断的分析类，增加总结

--- arithmetic/src/main/java/com/wing/lynne/lockdemo/LockInterruptDemo.java
+++ arithmetic/src/main/java/com/wing/lynne/lockdemo/LockInterruptDemo.java
@@ -11,19 +11,23 @@ public class LockInterruptDemo {
         ReentrantLock lock = new ReentrantLock();
         lock.lock();
         Thread t1 = new Thread(() -> {
+
 //            lock.lock();
             try {
+
                 lock.lockInterruptibly();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
+
                 threadName = Thread.currentThread().getName();
                 System.out.println(Thread.currentThread().getName() + " wake up from time waiting");
-            }  finally {
-                System.out.println("going to unlock");
-                lock.unlock();
+            } catch (Exception e) {
+
+                System.out.println(Thread.currentThread().getName() + " catch exception");
+
+            } finally {
+
+
             }
+
             System.out.println(Thread.currentThread().getName() + " going to die");
         }, "t1");
         t1.start();
@@ -31,4 +35,11 @@ public class LockInterruptDemo {
         t1.interrupt();
 
     }
+
 }
+
+/**
+ * 总结
+ *  lock.lock不响应中断
+ *  lock.lockInterruptibly会响应中断，并从当前行抛出一个中断异常，后续代码根据异常来进行特殊处理
+ */
