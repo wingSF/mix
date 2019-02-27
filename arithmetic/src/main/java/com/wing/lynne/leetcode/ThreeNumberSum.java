c1a8447d67142c3a771047544467e62fcaf241b6
wing <18735117232@163.com>
2019-02-27 12:01:58 +0800

    3数求和

--- arithmetic/src/main/java/com/wing/lynne/leetcode/ThreeNumberSum.java
+++ arithmetic/src/main/java/com/wing/lynne/leetcode/ThreeNumberSum.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * https://leetcode-cn.com/problems/3sum/
+ *
+ * 执行用时: 95 ms, 在3Sum的Java提交中击败了55.27% 的用户
+ * 内存消耗: 59.9 MB, 在3Sum的Java提交中击败了8.03% 的用户
+ * 执行用时: 94 ms, 在3Sum的Java提交中击败了56.87% 的用户
+ * 内存消耗: 57.3 MB, 在3Sum的Java提交中击败了9.18% 的用户
+ */
 public class ThreeNumberSum {
 
     public static void main(String[] args) {
@@ -15,7 +23,7 @@ public class ThreeNumberSum {
 //        int[] arrays = {-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6};
 //        int[] arrays = {1, 1, -2};
 //        int[] arrays = {0, 0, 0, 0};
-        int[] arrays = {3,0,-2,-1,1,2};
+        int[] arrays = {3, 0, -2, -1, 1, 2};
 //        int[] arrays = {-1, -2, -3, 4, 1, 3, 0, 3, -2, 1, -2, 2, -1, 1, -5, 4, -3};
 
         List<List<Integer>> lists = threeSum(arrays);
@@ -32,50 +40,35 @@ public class ThreeNumberSum {
         //对数组进行排序  O(logN)
         Arrays.sort(nums);
 
-        //遍历数组，寻找0的位置  O(n)
+        //遍历数组，寻找  O(n)
         for (int i = 0; i < nums.length - 1; i++) {
 
-            //去除重复
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+            if (i != 0) {
+                if (nums[i] == nums[i - 1]) {
+                    continue;
+                }
             }
 
-            //遍历的过程中寻找和为0的组合 todo 当前实现有问题
-            int left = i + 1;
-            int right = nums.length - 1;
-            boolean flag = true;
+            int start = i + 1;
+            int end = nums.length - 1;
 
-            while (left != right) {
+            while (start < end) {
 
-                if (nums[i] + nums[left] + nums[right] == 0) {
-
-                    List<Integer> resultList = new ArrayList<>();
-                    resultList.add(nums[i]);
-                    resultList.add(nums[left]);
-                    resultList.add(nums[right]);
-
-                    result.add(resultList);
-
-                    while (true) {
-                        if (left < right && nums[left] == nums[left + 1] && left + 1 != right) {
-                            left++;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    left++;
-                    flag = false;
-
+                if (nums[start] + nums[end] < -nums[i]) {
+                    start++;
+                } else if (nums[start] + nums[end] > -nums[i]) {
+                    end--;
                 } else {
+                    List<Integer> singleResult = new ArrayList<>();
+                    singleResult.add(nums[i]);
+                    singleResult.add(nums[start]);
+                    singleResult.add(nums[end]);
 
-                    if (flag) {
-                        left++;
-                        flag = false;
-                    } else {
-                        right--;
-                        flag = true;
-                    }
+                    result.add(singleResult);
+
+                    do {
+                        start++;
+                    } while (start <= nums.length - 1 && nums[start] == nums[start - 1]);
                 }
 
             }
@@ -86,3 +79,38 @@ public class ThreeNumberSum {
         return result;
     }
 }
+
+
+/**
+ * @author 王龙
+ * var threeSum = function(nums) {
+ * nums.sort(function(a,b){
+ * return a-b;
+ * })
+ * var target,result=[],len=nums.length;
+ * for(var i=0;i<len;i++){
+ * if (target===nums[i]){
+ * continue;
+ * }
+ * else{
+ * target=nums[i];
+ * }
+ * var low=i+1,high=len-1;
+ * while(low<high){
+ * if(nums[low]+nums[high]===-target){
+ * result.push([target,nums[low],nums[high]]);
+ * do{
+ * low++;
+ * }while(nums[low]===nums[low-1]);
+ * }
+ * else if(nums[low]+nums[high]<-target){
+ * low++;
+ * }
+ * else{
+ * high--;
+ * }
+ * }
+ * }
+ * return result;
+ * };
+ */
