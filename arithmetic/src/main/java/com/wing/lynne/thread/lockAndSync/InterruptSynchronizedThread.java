5ddbaac59e0606057c591ff7792a4b6976c1e137
wing-SF <18735117232@163.com>
2019-05-22 19:26:13 +0800

    docs:sync与lock对比|Condition到底是什么
    sync与lock对比
    - 通过代码的形式总结sync与lock在遇见异常和遇见中断时候的处理
    
    Condition
    - 通过对javadoc的解读，找到condition对象的本质意义
    
    good good study ，day day up.....

--- arithmetic/src/main/java/com/wing/lynne/thread/lockAndSync/InterruptSynchronizedThread.java
+++ arithmetic/src/main/java/com/wing/lynne/thread/lockAndSync/InterruptSynchronizedThread.java
@@ -6,56 +6,149 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 为了演示lock与sync对与interrupt的响应
- * 但是没有得到结果
- * todo
- *
+ * <p>
+ * 以下代码演示了synchronized和lock在遇到InterruptException时的处理机制
+ * <p>
+ * synchronize会自动释放锁
+ * lock必须手动调用unlock方法，才能释放
  */
 public class InterruptSynchronizedThread {
 
     public static void main(String[] args) throws InterruptedException {
 
+//        reactForException();
+
+        reactForInterrupt();
+        return;
+
+    }
+
+    private static void reactForInterrupt() throws InterruptedException {
+
         final Object object = new Object();
 
+        Thread syncThread1 = new Thread(() -> {
+            synchronized (object) {
+                while (true) {
+                    try {
+                        System.out.println(Thread.currentThread() + " 获取同步对象成功");
+                        TimeUnit.HOURS.sleep(1);
+                    } catch (InterruptedException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        }, "syncThread1");
+
+        Thread syncThread2 = new Thread(() -> {
+            synchronized (object) {
+                System.out.println(Thread.currentThread() + " 获取同步对象成功");
+            }
+        }, "syncThread2");
+
+        Lock lock = new ReentrantLock();
+
+        Thread lockThread1 = new Thread(() -> {
+
+            lock.lock();
+
+            try {
+                System.out.println(Thread.currentThread() + " 获取锁对象成功");
+                while (true) {
+                    TimeUnit.HOURS.sleep(1);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }, "lockThread1");
+
+        Thread lockThread2 = new Thread(() -> {
+
+//            此处使用lock.lock()将不会响应中断，使用lock.lockInterruptibly()则会抛出中断异常，伺机处理
+            lock.lock();
+//            try {
+//                lock.lockInterruptibly();
+//            } catch (InterruptedException e) {
+//                System.out.println(Thread.currentThread()+" 获取锁失败");
+//                return;
+//            }
+            try {
+                System.out.println(Thread.currentThread() + " 获取锁对象成功 ");
+            } finally {
+                lock.unlock();
+            }
+
+
+        }, "lockThread2");
+
+        syncThread1.start();
+        lockThread1.start();
+        syncThread2.start();
+        lockThread2.start();
+
+
+        TimeUnit.SECONDS.sleep(10);
+
+        syncThread2.interrupt();
+        lockThread2.interrupt();
+
+    }
+
+    private static void reactForException() throws InterruptedException {
+        final Object object = new Object();
 
         Thread syncThread1 = new Thread(() -> {
 
-            synchronized (object){
-                while(!Thread.currentThread().isInterrupted()){
-                    System.out.println(Thread.currentThread().getName()+" sleeping ");
-                    try {
-                        TimeUnit.SECONDS.sleep(3);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    synchronized (object) {
+                        System.out.println(Thread.currentThread().getName() + " sleeping ");
+
+                        if (System.currentTimeMillis() % 2 == 0) {
+                            throw new InterruptedException();
+                        }
                     }
+                    TimeUnit.SECONDS.sleep(3);
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
         }, "syncThread1");
 
         Thread syncThread2 = new Thread(() -> {
 
-            synchronized (object){
-                while(!Thread.currentThread().isInterrupted()){
-                    System.out.println(Thread.currentThread().getName()+" sleeping ");
-                    try {
-                        TimeUnit.SECONDS.sleep(3);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    synchronized (object) {
+                        System.out.println(Thread.currentThread().getName() + " sleeping ");
                     }
+                    TimeUnit.SECONDS.sleep(3);
                 }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-
         }, "syncThread2");
 
         Lock lock = new ReentrantLock();
 
-        Thread lockThread1= new Thread(()->{
+        Thread lockThread1 = new Thread(() -> {
 
-            lock.lock();
-
-            while(!Thread.currentThread().isInterrupted()){
-                System.out.println(Thread.currentThread().getName()+" sleeping ");
+            while (!Thread.currentThread().isInterrupted()) {
                 try {
+                    //注意lock.lock()方法应该放置在try-catch-finally块外边，防止获取锁异常时，导致被解锁，此处只是为了演示
+                    lock.lock();
+                    System.out.println(Thread.currentThread().getName() + " sleeping ");
+
+                    if (System.currentTimeMillis() % 2 == 0) {
+                        throw new InterruptedException();
+                    }
+
+                    //当发生中断异常的时候，不会调用下面这个unlock(),所以锁没有释放，导致lockThread2线程无法获取锁
+                    lock.unlock();
+                    //另注，这是一种错误的写法，只是为了达到演示的效果，正确的应该将unlock语句放到finally代码块中执行，可参见lockThread2的unlock调用
                     TimeUnit.SECONDS.sleep(3);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -63,23 +156,25 @@ public class InterruptSynchronizedThread {
             }
 
 
-        },"lockThread1");
+        }, "lockThread1");
 
-        Thread lockThread2 = new Thread(()->{
+        Thread lockThread2 = new Thread(() -> {
 
-            lock.lock();
 
-            while(!Thread.currentThread().isInterrupted()){
-                System.out.println(Thread.currentThread().getName()+" sleeping ");
+            while (!Thread.currentThread().isInterrupted()) {
+                lock.lock();
                 try {
+                    System.out.println(Thread.currentThread().getName() + " sleeping ");
                     TimeUnit.SECONDS.sleep(3);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
+                } finally {
+                    lock.unlock();
                 }
             }
 
 
-        },"lockThread2");
+        }, "lockThread2");
 
         syncThread1.start();
         syncThread2.start();
@@ -88,8 +183,5 @@ public class InterruptSynchronizedThread {
 
 
         TimeUnit.SECONDS.sleep(10);
-
-        syncThread1.interrupt();
-        lockThread1.interrupt();
     }
 }
--- studyNote/src/main/resources/同步器/AbstractQueuedSynchronizer升级篇.md
+++ studyNote/src/main/resources/同步器/AbstractQueuedSynchronizer升级篇.md
@@ -8,3 +8,12 @@
     * add线程虽然被阻塞，但他一直在等待一个时机，那就是有读线程把数据取走了，队列有空间可以写了
     * read线程虽然被阻塞，但也在等待一个时间，那就是有数据被加入到有界队列中
     * 所以会有一种需求，当向有界队列中成功添加元素后，要唤醒一个等待的读线程取走数据。同理，当读取到空的时候，需要唤醒一个add线程，添加数据。
+* 问:到底什么东西是个Condition？
+    * 终于在Condition接口的注释中找到了答案
+    > Where a {@code Lock} replaces the use of {@code synchronized} methods and statements  
+     a {@code Condition} replaces the use of the Object monitor methods.
+    * lock取代了synchronzed关键字，condition则取代了wait/notify/notifyAll的通知机制
+* lock/condition既然能取代synchronized/wait/notify，那一定是原来的实现有问题
+    * 问题
+        * synchronized遇到异常会自动释放锁，改进后的lock必须调用unlock方法才可以
+        * synchronized不会响应中断，lock有响应中断的api，甚至有等待超时的api，这样对于锁的控制将会更加灵活。 
