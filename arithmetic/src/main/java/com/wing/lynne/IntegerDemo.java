22af9bbec3e5d0f3d7795dbbe6f1a6615031a6a5
wing-SF <18735117232@163.com>
2019-06-27 20:53:34 +0800

    docs:数值比较框架
    good good study ，day day up.....

---
+++ arithmetic/src/main/java/com/wing/lynne/IntegerDemo.java
@@ -0,0 +1,38 @@
+package com.wing.lynne;
+
+/**
+ * 演示
+ */
+public class IntegerDemo {
+
+    public static void main(String[] args) {
+        String str = "wing";
+        System.out.println("main before addStr address = " + str.hashCode());
+        String resultStr = addStr(str);
+        System.out.println("main after addStr address = " + str);
+        System.out.println(str == resultStr);
+
+        Integer paramIndex = 200;
+        System.out.println("main before incr address = " + paramIndex.hashCode());
+        Integer resultParamIndex = incr(paramIndex);
+        System.out.println("main after incr address = " + paramIndex);
+        System.out.println(paramIndex == resultParamIndex);
+    }
+
+    private static String addStr(String str) {
+        System.out.println("before addStr address = " + str.hashCode());
+        str += "lynne";
+        System.out.println("after addStr address = " + str.hashCode());
+        System.out.println("addStr:" + str);
+        return str;
+    }
+
+    private static Integer incr(Integer paramIndex) {
+        System.out.println("before incr address = " + paramIndex.hashCode());
+        paramIndex++;
+        System.out.println("incr:" + paramIndex);
+        System.out.println("after incr address = " + paramIndex.hashCode());
+        return paramIndex;
+    }
+
+}
