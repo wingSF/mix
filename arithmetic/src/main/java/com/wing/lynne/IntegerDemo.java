787a0eef5996ec99029f9dccad62185a5532a2fe
wuning <18735117232@163.com>
2019-06-28 17:44:23 +0800

    feat：Integer相关功能
    Integer相关功能
    - Integer在作为对象传递的时候，总是传递的对象
    - 凡事对Integer的value属性做变更，都会导致重新创建一个新对象
      所以Integer作为参数传入方法，一旦被修改，方法外是无法获得结果的
    - unsafe类除外，unsafe可以直接修改被final标识的字段value值，但这个时候是不会生成新的对象

--- arithmetic/src/main/java/com/wing/lynne/IntegerDemo.java
+++ arithmetic/src/main/java/com/wing/lynne/IntegerDemo.java
@@ -1,39 +1,77 @@
 package com.wing.lynne;
 
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
 /**
  * 演示参数参数对象类型，且对象类型属于Immutable类型
  * 该类型的对象，每次value发生变更都会重新生成一个对象
+ *
+ * 在#incrByUnsafe方法中，使用unsafe类直接对对象的value值进行修改，这样可以证明一句话
+ *  在java中，基本类型数据作为参数传递，传值，对象类型数据作为参数传递，传对象的引用|地址值
+ *
+ * 要特别注意Integer的缓冲池，一旦被修改缓冲池中的对象value成功，别人会很迷惑
+ *  效果是只要用到装箱的地方，实际的值总会与预期的不一致
+ *
+ * unsafe类除外，unsafe可以直接修改被final标识的字段value值，但这个时候是不会生成新的对象
+ *
  */
 public class IntegerDemo {
 
-    public static void main(String[] args) {
-        String str = "wing";
-        System.out.println("main before addStr address = " + str.hashCode());
-        String resultStr = addStr(str);
-        System.out.println("main after addStr address = " + str);
-        System.out.println(str == resultStr);
+    private static Unsafe unsafe = null;
+
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
 
         Integer paramIndex = 200;
-        System.out.println("main before incr address = " + paramIndex.hashCode());
         Integer resultParamIndex = incr(paramIndex);
-        System.out.println("main after incr address = " + paramIndex);
         System.out.println(paramIndex == resultParamIndex);
+        System.out.println("paramIndex = " + paramIndex);
+
+        Integer paramIndex1 = 300;
+        Integer paramIndex1Result = incrByUnSafe(paramIndex1);
+        System.out.println(paramIndex1 == paramIndex1Result);
+        System.out.println("paramIndex1 = " + paramIndex1);
+
+        Integer numberInIntegerCache = 10;
+        Integer incrByUnSafe = incrByUnSafe(numberInIntegerCache);
+        System.out.println(numberInIntegerCache==incrByUnSafe);
+        System.out.println(incrByUnSafe);
+        Integer numberTemp = 10;
+        System.out.println(numberTemp);
+
+
     }
 
-    private static String addStr(String str) {
-        System.out.println("before addStr address = " + str.hashCode());
-        str += "lynne";
-        System.out.println("after addStr address = " + str.hashCode());
-        System.out.println("addStr:" + str);
-        return str;
+    private static Integer incrByUnSafe(Integer paramIndex) throws NoSuchFieldException, IllegalAccessException {
+
+        getUnsafe();
+
+        long valueOffset = unsafe.objectFieldOffset(Integer.class.getDeclaredField("value"));
+
+        unsafe.putInt(paramIndex, valueOffset, paramIndex.intValue()+1);
+
+        return paramIndex;
+
     }
+
 
     private static Integer incr(Integer paramIndex) {
-        System.out.println("before incr address = " + paramIndex.hashCode());
         paramIndex++;
-        System.out.println("incr:" + paramIndex);
-        System.out.println("after incr address = " + paramIndex.hashCode());
         return paramIndex;
     }
 
+    private static void getUnsafe() throws NoSuchFieldException, IllegalAccessException {
+
+        if (unsafe == null) {
+            Class<Unsafe> unsafeClass = Unsafe.class;
+
+            Field unsafeField = unsafeClass.getDeclaredField("theUnsafe");
+
+            unsafeField.setAccessible(true);
+
+            unsafe = (Unsafe) unsafeField.get(unsafeClass);
+        }
+
+    }
 }
