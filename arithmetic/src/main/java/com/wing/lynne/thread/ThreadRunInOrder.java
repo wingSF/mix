ecae5cf1af831f821957acde17c87c855ab78701
wuning <18735117232@163.com>
2019-09-24 12:58:29 +0800

    feat:更新顺序执行线程代码
    - 抽取runnable逻辑
    - 增加启动先后顺序控制
    
    good good study，day day up！

--- arithmetic/src/main/java/com/wing/lynne/thread/ThreadRunInOrder.java
+++ arithmetic/src/main/java/com/wing/lynne/thread/ThreadRunInOrder.java
@@ -1,111 +1,91 @@
 package com.wing.lynne.thread;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadRunInOrder {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Lock lock = new ReentrantLock();
         Condition condition1 = lock.newCondition();
         Condition condition2 = lock.newCondition();
         Condition condition3 = lock.newCondition();
+        int printTime = 10;
 
-        new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+        new Thread(new Print(lock, condition1, condition2, printTime), "a").start();
+        new Thread(new Print(lock, condition2, condition3, printTime), "b").start();
+        new Thread(new Print(lock, condition3, condition1, printTime), "c").start();
 
-                boolean lockResult = lock.tryLock();
-
-                if (lockResult) {
-
-                    try {
-                        try {
-                            condition1.await();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        System.out.println(Thread.currentThread().getName());
-                        condition2.signal();
-                    } catch (Exception e) {
-
-                    } finally {
-                        lock.unlock();
-                    }
-                } else {
-                    i--;
-                }
-
-
+        //这段代码用来获取第一个a线程对象
+        Thread aThread = null;
+        for (Thread thread : Thread.getAllStackTraces().keySet()) {
+            if (thread.getName().equals("a")) {
+                aThread = thread;
+                break;
             }
-        }, "a").start();
+        }
 
-        new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
+        //判断a线程对象的状态是等待，再执行唤醒，避免main执行signal的时候，a线程还没有出于wait状态，出现上述情况，程序会假死
+        while (aThread.getState() != Thread.State.WAITING) {
+            TimeUnit.SECONDS.sleep(1);
+        }
 
-                boolean lockResult = lock.tryLock();
-
-                if (lockResult) {
-
-                    try {
-                        try {
-                            condition2.await();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        System.out.println(Thread.currentThread().getName());
-                        condition3.signal();
-                    } catch (Exception e) {
-
-                    } finally {
-                        lock.unlock();
-                    }
-                } else {
-                    i--;
-                }
-
-
-            }
-        }, "b").start();
-
-        new Thread(() -> {
-            for (int i = 0; i < 10; i++) {
-
-                boolean lockResult = lock.tryLock();
-
-                if (lockResult) {
-
-                    try {
-                        try {
-                            condition3.await();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        System.out.println(Thread.currentThread().getName());
-                        condition1.signal();
-                    } catch (Exception e) {
-
-                    } finally {
-                        lock.unlock();
-                    }
-                } else {
-                    i--;
-                }
-
-            }
-        }, "c").start();
-
-        while(lock.tryLock()){
+        while (lock.tryLock()) {
             try {
                 condition1.signal();
-            }catch (Exception e){
+            } catch (Exception e) {
 
-            }finally {
+            } finally {
                 lock.unlock();
             }
             break;
         }
 
+    }
+
+    static class Print implements Runnable {
+
+        private Lock lock;
+        private Condition waitCondition;
+        private Condition signalCondition;
+        private int printTime;
+
+        public Print(Lock lock, Condition waitCondition, Condition signalCondition, int printTime) {
+            this.lock = lock;
+            this.waitCondition = waitCondition;
+            this.signalCondition = signalCondition;
+            this.printTime = printTime;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < printTime; i++) {
+
+                boolean lockResult = lock.tryLock();
+
+                if (lockResult) {
+
+                    try {
+                        try {
+                            waitCondition.await();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(Thread.currentThread().getName());
+                        signalCondition.signal();
+                    } catch (Exception e) {
+
+                    } finally {
+                        lock.unlock();
+                    }
+                } else {
+                    i--;
+                }
+
+            }
+        }
     }
 }
