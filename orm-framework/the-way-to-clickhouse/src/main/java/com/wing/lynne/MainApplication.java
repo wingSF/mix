56623a9d4908247cb58e0cbba58d9503f6aa26aa
WUNING <wuning@kingsoft.com>
2020-04-19 06:42:55 +0800

    feat:复现clickhouse数据源，连接长时间不释放现象

--- orm-framework/the-way-to-clickhouse/src/main/java/com/wing/lynne/MainApplication.java
+++ orm-framework/the-way-to-clickhouse/src/main/java/com/wing/lynne/MainApplication.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 官网地址
@@ -14,14 +15,31 @@ public class MainApplication {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        Class.forName("com.github.housepower.jdbc.ClickHouseDriver");
-        Connection connection = DriverManager.getConnection("jdbc:clickhouse://127.0.0.1:32769/rds_uf_union_order");
+        Class.forName("cc.blynk.clickhouse.ClickHouseDriver");
+        Connection connection = DriverManager.getConnection("jdbc:clickhouse://${}:${port}/${dbname}","${username}","${password}");
 
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("show tables");
+        ResultSet resultSet = statement.executeQuery("${sql}");
         while (resultSet.next()) {
             String string = resultSet.getString(1);
             System.out.println(string);
+            string = resultSet.getString(2);
+            System.out.println(string);
+            string = resultSet.getString(3);
+            System.out.println(string);
+            string = resultSet.getString(4);
+            System.out.println(string);
+            string = resultSet.getString(5);
+            System.out.println(string);
+            string = resultSet.getString(6);
+            System.out.println(string);
+
+            try {
+                TimeUnit.MINUTES.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
 
 
